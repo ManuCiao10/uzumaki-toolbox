81ac93c0a4ca66862bddc1b9fbdc89739270abda
admin <emanuele.ardinghi@gmail.com>
2023-03-10 01:24:34 +0100

    fixed ups pick-up

--- internal/pickup.py
+++ internal/pickup.py
@@ -417,7 +417,6 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     except:
         pass
 
-    print_task("Payment submitted!", GREEN)
 
     headers = {
     'authority': 'wwwapps.ups.com',
@@ -439,10 +438,10 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'x-requested-with': 'XMLHttpRequest',
 }
 
-    data = {
+    payload_cc = {
     'loc': 'it_IT',
     'op': 'NEXT',
-    'billingindicator': '04',
+    'billingindicator': hargeTypeCode,
     'paymentYourUPSAccountNumber': '',
     'paymentUPSPostalCode': '',
     'paymentThirdPartybillingIndicator': 'billThirdForPickupCharges',
@@ -455,7 +454,7 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'otherPaymentType': 'Card',
     'pre': 'cpcWidget',
     'rapp': 'CPC',
-    'cct': '02_04',
+    'cct': f"{paymentMediaTypeCode}_{hargeTypeCode}",
     'pmt': '2',
     'apy': 'true',
     'ptr': 'false',
@@ -471,11 +470,11 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'cpcWidgetCountryCashPostalCode': '',
     'cpcWidgetCountryCashCity': '',
     'cpcWidgetCountryCashStates': '',
-    'ccn': '5354564980016154',
+    'ccn': credit_card,
     'sccn': '',
-    'svc': '199',
-    'exm': '2',
-    'exy': '2026',
+    'svc': month,
+    'exm': month,
+    'exy': year,
     'eba': '',
     'cba': '',
     'sba': '',
@@ -488,26 +487,26 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'cpcWidgetCountryCity': '',
     'cpcWidgetCountryStates': '',
     'addressSelect': 'newAddress',
-    'cpcWidgetFirstName': 'emanuele',
-    'cpcWidgetLastName': 'ardinghi',
+    'cpcWidgetFirstName': name,
+    'cpcWidgetLastName': surname,
     'cpcWidgetCountry': 'IT',
-    'cpcWidgetAddress1': 'via orcagna 66',
+    'cpcWidgetAddress1': address,
     'cpcWidgetAddress2': '',
     'cpcWidgetAddress3': '',
-    'cpcWidgetPostalCode': '50121',
-    'cpcWidgetCity': 'firenze',
-    'cpcWidgetStates': 'Agrigento',
+    'cpcWidgetPostalCode': zip_code,
+    'cpcWidgetCity': city,
+    'cpcWidgetStates': city,
     'taxIdTypeCode': '0005',
-    'taxId': 'RDNMNL02T07D612C',
-    'pecId': 'emanuele.ardinghi@gmail.com',
-    'sdiId': 'emanuele.ardinghi@gmail.com',
+    'taxId': tax_id,
+    'pecId': tax_id,
+    'sdiId': tax_id,
     'tracknumlist': '',
 }
 
     response = session.post(
     'https://wwwapps.ups.com/pickup/api/service/validate/payment',
     headers=headers,
-    data=data,
+    data=payload_cc,
 )
 
     data_ = response.json()
@@ -521,7 +520,6 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'no-cache',
-    # 'cookie': 'com.ups.pickup.cb.sData=9c4b5ebaedfc40e8bb1896b25fa2d71f:kfVr0QH3Tq2cPNbfivAKyQYD8xiiMtBq3bxdYbyhWQg=; ups_language_preference=it_IT; PIM-SESSION-ID=HsQVb9cxHucp9q5C; bm_sz=CB528A049C34C35AA336F4F481F94470~YAAQVH4ZuAkDbcKGAQAAbX0ByBPyoIoTZnu/39L1YkPmNPRUcIZ7abFdk/ueeqywKwnRlamiVQ9rbRgyg+pJUB12lKlGzoytkbhwPTrBLpjI5kksbDd1wERQo6whFF9UywLcPs9zxLo5dSVnW4FCoTqYuiAgE0nkKC+lhZceW25u+ckr246yQvBM48b++L41xL1obqrseL1bv4TM+gJW8PqeC0BgwCRACbHgM6hseCPkvZoiCjVfFwNG94ObMD/tBQIYmt6jT5+7U1IMKB1Mn5D0UstStxRtuJCafUW9jqE=~3485763~4605497; sharedsession=b897e593-da08-4f11-bfdd-28c2ea292c92:w; AKA_A2=A; _abck=22D48C5448777CF224D9F73DA0194E17~0~YAAQgTRoaKVcRL6GAQAAtfOFyAlfx/iQletw42iEWhgltvhdWx/CXzkL5pBP4Cr0dGr8xN759vplmKyE9dscOcsO0arpoDFMWEQmdMLR8pcpfNKz2EZxKjsf0Hmq5AkHz3rRIRg3p0+IoLezIrH9DbO9vFP5t6hlLJ6NXLjJCTQ+M+chGoMFu0M3i4elF+nuY2kY+jwS0GCDYHI9Fufq8Fh+3+wjQnk3NIWjCMEJdTvikZhF9a6BCkgl3BN3uoH7fLP0jKUbRi3Vf08wam0E6ifQBNGDyL7f27SqON6cZZdRn7l64rrA5tp6jgyWh4Wq94kKCYepLVJ2D7rrjPHHIofdjK7Ubr7yjltCMw7ntfiJG87KGRTV6Z4GFSiNHgkUYCSmIjN99RYGnJVIxotwqNhoPtIf~-1~-1~1678404971; utag_main=v_id:0186c50a393b00385f9fcffc36b004075003106d0093c$_sn:5$_se:9$_ss:0$_st:1678403267081$dc_visit:5$ses_id:1678397910882%3Bexp-session$_pn:9%3Bexp-session$fs_sample_user:false%3Bexp-session$dc_event:9%3Bexp-session; ak_bmsc=D99B98E3557651362E083AB119BA059D~000000000000000000000000000000~YAAQgTRoaMpcRL6GAQAAYf6FyBNkX970HqSDcemq0Bblh3ovbHm38IqmOTU15wPPxXy6G3jPbPk5hlosGHI37hlensidQ6L1qf7NdeLBRCGizvqSqfdp2u9BPnm5ujKX6HEcJL7Ab8v7yvVSdxsLlQxiPdbm6jqJz2IEknJVw/s8wuE7b8D2GV1PynzBgfl0Jl4JSjxK9XO3GNdXie+L3pWTTPFW+sQw1P3E9nB0FbKuVsutMYovWQ3+RgEJDOQiQ3AUYIW0T/JcTGYYN8eGe9kvVmKd9/zyTJPhiYwK98OtkZfpXylaqo81x3rcdD9iSrMBOUT8C4h9DaGCwgC8VGaWhZJ0AULf6T82EtBf6i4RZLYPU5skLUKGIkdLAjlxLvC1QcDe+6TNsjtoppEMglvyLhkSH2PlVEoUC7KO5U/9OQggle1Z7BTrUbZJ4RXpWFuNF5vaCEl6nSS+9aEmT4PsPfVzjolhiFbj2sdcJB0CCu7P4WwtsA==',
     'origin': 'https://wwwapps.ups.com',
     'pragma': 'no-cache',
     'referer': 'https://wwwapps.ups.com/pickup/processinfo?loc=it_IT',
@@ -537,73 +535,9 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
 }
 
-    data = {
-    'loc': 'it_IT',
-    'op': 'NEXT',
-    'billingindicator': '04',
-    'paymentYourUPSAccountNumber': '',
-    'paymentUPSPostalCode': '',
-    'paymentThirdPartybillingIndicator': 'billThirdForPickupCharges',
-    'paymentThirdPartyAccountNumber': '',
-    'paymentReceiverCountry': 'IT',
-    'paymentThirdPartyCountry': 'IT',
-    'paymentThirdPartyPostalCode': '',
-    'paymentMethodValidated': 'true',
-    'paymentCard': '',
-    'otherPaymentType': 'Card',
-    'pre': 'cpcWidget',
-    'rapp': 'CPC',
-    'cct': '02_04',
-    'pmt': '2',
-    'apy': 'true',
-    'ptr': 'false',
-    'spm': '',
-    'dpm': '',
-    'epm': 'false',
-    'is3d': '',
-    'accountNumber': '',
-    'accountName': '',
-    'IT': 'IT',
-    'null': '',
-    'cpcWidgetCountryCashCountry': 'IT',
-    'cpcWidgetCountryCashPostalCode': '',
-    'cpcWidgetCountryCashCity': '',
-    'cpcWidgetCountryCashStates': '',
-    'ccn': '5354564980016154',
-    'sccn': '',
-    'svc': '199',
-    'exm': '2',
-    'exy': '2026',
-    'eba': '',
-    'cba': '',
-    'sba': '',
-    'baid': '',
-    'pid': '',
-    'pem': '',
-    'ppan': '',
-    'cpcWidgetCountryCountry': 'IT',
-    'cpcWidgetCountryPostalCode': '',
-    'cpcWidgetCountryCity': '',
-    'cpcWidgetCountryStates': '',
-    'addressSelect': 'newAddress',
-    'cpcWidgetFirstName': 'emanuele',
-    'cpcWidgetLastName': 'ardinghi',
-    'cpcWidgetCountry': 'IT',
-    'cpcWidgetAddress1': 'via orcagna 66',
-    'cpcWidgetAddress2': '',
-    'cpcWidgetAddress3': '',
-    'cpcWidgetPostalCode': '50121',
-    'cpcWidgetCity': 'firenze',
-    'cpcWidgetStates': 'Agrigento',
-    'taxIdTypeCode': '0005',
-    'taxId': 'RDNMNL02T07D612C',
-    'pecId': 'emanuele.ardinghi@gmail.com',
-    'sdiId': 'emanuele.ardinghi@gmail.com',
-    'tracknumlist': '',
-}
 
-    response = session.post('https://wwwapps.ups.com/pickup/processpaymentdetail', headers=headers, data=data)
-    # print(response.text)
+    response = session.post('https://wwwapps.ups.com/pickup/processpaymentdetail', headers=headers, data=payload_cc)
+
     if "Nome referente:" in response.text:
         print("OK")
     else:
@@ -656,7 +590,6 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'no-cache',
-    # 'cookie': 'JSESSIONID=AC1FB2B53FF8D6BECAB2B7FB78079ED3; .AspNetCore.Antiforgery.XlURHxPT8_I=CfDJ8H1SCHo8oO5Ascce1J42d2D6fIPzHeg3ZFicAqZ8baHosXPMw0t1rDtl6sSO7xkEyhWiyDSorPHWdfleYbdWcA7DH4Hgy2OaQxsg10unKw5kchiVupZSbnisXHtIEXxUjfS-AVYPVfXrsX95xVTTT-A; .AspNetCore.Antiforgery.pKFBCrPAOmA=CfDJ8H1SCHo8oO5Ascce1J42d2Cgfn0OxlXNuFNMWE5cJZFkZ5PBRNxfAnechdZcNqTJJj8cuDIn8SUAUmBnSrjUKn2sV1CmZM9snEOfzlVwEjJwCBSgzRmQfrOJSeswJcDa1YhIldb2ASoco-YPPiVwPuA; gig_canary=false; gig_canary_ver=13549-3-27869190; GPC_cookie_corrected=true, ts:1678332400820,c3: 0,c7: 0,preferences; ups_language_preference=it_IT; PIM-SESSION-ID=HsQVb9cxHucp9q5C; bm_sz=CB528A049C34C35AA336F4F481F94470~YAAQVH4ZuAkDbcKGAQAAbX0ByBPyoIoTZnu/39L1YkPmNPRUcIZ7abFdk/ueeqywKwnRlamiVQ9rbRgyg+pJUB12lKlGzoytkbhwPTrBLpjI5kksbDd1wERQo6whFF9UywLcPs9zxLo5dSVnW4FCoTqYuiAgE0nkKC+lhZceW25u+ckr246yQvBM48b++L41xL1obqrseL1bv4TM+gJW8PqeC0BgwCRACbHgM6hseCPkvZoiCjVfFwNG94ObMD/tBQIYmt6jT5+7U1IMKB1Mn5D0UstStxRtuJCafUW9jqE=~3485763~4605497; sharedsession=b897e593-da08-4f11-bfdd-28c2ea292c92:w; JSESSIONID=D0A48FDE522AA84A9538F9F684003C21; _abck=22D48C5448777CF224D9F73DA0194E17~0~YAAQgTRoaKVcRL6GAQAAtfOFyAlfx/iQletw42iEWhgltvhdWx/CXzkL5pBP4Cr0dGr8xN759vplmKyE9dscOcsO0arpoDFMWEQmdMLR8pcpfNKz2EZxKjsf0Hmq5AkHz3rRIRg3p0+IoLezIrH9DbO9vFP5t6hlLJ6NXLjJCTQ+M+chGoMFu0M3i4elF+nuY2kY+jwS0GCDYHI9Fufq8Fh+3+wjQnk3NIWjCMEJdTvikZhF9a6BCkgl3BN3uoH7fLP0jKUbRi3Vf08wam0E6ifQBNGDyL7f27SqON6cZZdRn7l64rrA5tp6jgyWh4Wq94kKCYepLVJ2D7rrjPHHIofdjK7Ubr7yjltCMw7ntfiJG87KGRTV6Z4GFSiNHgkUYCSmIjN99RYGnJVIxotwqNhoPtIf~-1~-1~1678404971; ak_bmsc=D99B98E3557651362E083AB119BA059D~000000000000000000000000000000~YAAQgTRoaMpcRL6GAQAAYf6FyBNkX970HqSDcemq0Bblh3ovbHm38IqmOTU15wPPxXy6G3jPbPk5hlosGHI37hlensidQ6L1qf7NdeLBRCGizvqSqfdp2u9BPnm5ujKX6HEcJL7Ab8v7yvVSdxsLlQxiPdbm6jqJz2IEknJVw/s8wuE7b8D2GV1PynzBgfl0Jl4JSjxK9XO3GNdXie+L3pWTTPFW+sQw1P3E9nB0FbKuVsutMYovWQ3+RgEJDOQiQ3AUYIW0T/JcTGYYN8eGe9kvVmKd9/zyTJPhiYwK98OtkZfpXylaqo81x3rcdD9iSrMBOUT8C4h9DaGCwgC8VGaWhZJ0AULf6T82EtBf6i4RZLYPU5skLUKGIkdLAjlxLvC1QcDe+6TNsjtoppEMglvyLhkSH2PlVEoUC7KO5U/9OQggle1Z7BTrUbZJ4RXpWFuNF5vaCEl6nSS+9aEmT4PsPfVzjolhiFbj2sdcJB0CCu7P4WwtsA==; AKA_A2=A; utag_main=v_id:0186c50a393b00385f9fcffc36b004075003106d0093c$_sn:5$_se:11$_ss:0$_st:1678403887471$dc_visit:5$ses_id:1678397910882%3Bexp-session$_pn:11%3Bexp-session$fs_sample_user:false%3Bexp-session$dc_event:11%3Bexp-session',
     'pragma': 'no-cache',
     'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Brave";v="110"',
     'sec-ch-ua-mobile': '?0',
@@ -672,14 +605,13 @@ def checkout(session: requests.Session, row: list, dropoffdate: str):
 
     ciao3 = {
     'type': '3d',
-    'transactionId': '1913693053',
+    'transactionId': transaction_id,
     'returnTo': 'https://wwwapps.ups.com/pickup/processcpcpayment?loc=it_IT',
 }
-    print(transaction_id)
+
     response = session.get('https://www.ups.com/cpcws/redirect', params=ciao3, headers=headers)
-    print(response.url)
-    print(response.status_code)
-    print(response.text)
+
+    print_task("Payment submitted!", GREEN)
 
 
 def schedule(row: list, session: requests.Session):
